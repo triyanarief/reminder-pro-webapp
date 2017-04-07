a12f54f36c5916645e84c7e766f549f18361a64b
triyanarief <triyanarief@icloud.com>
2017-04-07 09:12:10 +0700

    adding reminder setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    console.log('this state', this.state);
+  }
+
   render() {
     return(
       <div className="App">
@@ -10,9 +21,9 @@ class App extends Component {
         </div>
         <div className="form-inline">
           <div className="form-group">
-            <input className="form-control" placeholder="I Have To ..."/>
+            <input className="form-control" placeholder="I Have To ..." onChange={event => this.setState({text: event.target.value})} />
           </div>
-          <button type="button" className="btn btn-success">Add Reminder</button>
+          <button type="button" className="btn btn-success" onClick={() => this.addReminder()} >Add Reminder</button>
         </div>
       </div>
     )
